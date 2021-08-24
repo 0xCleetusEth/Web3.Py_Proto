08c71dd8fe9b4291c76e81887b5c72053efbbb99
Chevis || Cleetus <sandysallahurst@gmail.com>
2021-08-24 01:09:40 -0700

    do work

--- registry/ftmDict.py
+++ registry/ftmDict.py
@@ -2,9 +2,6 @@ import interfaces
 
 class SpookyswapStaking:
     
-    def __main__(self):
-        print('returning SpookyswapStaking attribute...')
-
     def getAddr():
         SpookyswapStakingAddress = '0x2b2929E785374c651a81A63878Ab22742656DcDd'
         return SpookyswapStakingAddress
