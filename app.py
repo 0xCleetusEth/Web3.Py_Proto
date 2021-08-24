2394e9f9c21d3c0baa4c1a6e665a697a76500bc2
Chevis || Cleetus <sandysallahurst@gmail.com>
2021-08-24 02:24:37 -0700

    p

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 import web3
-from registry import ftmDict
 from environment import getenv
 from scripts import connect, terminate
+from registry import ftmDict
 
 
 def toNormal(_inWei):
-    _inNormal = web3.fromWei(_inWei, 'ether')
+    _inNormal = w3.fromWei(_inWei, 'ether')
     return _inNormal
 
 # Establish choice of infura/chain here, connect...
 #connect.fantom(), connect.eth(), connect.matic()
-web3 = connect.fantom()
-myAddress = getenv.publicKey()
+w3 = connect.fantom()
+me = getenv.publicKey()
 
 
-balance = web3.eth.getBalance(myAddress)
+balance = w3.eth.getBalance(me)
 print(toNormal(balance))
     
 #initializes spookychef interface, checks ftm-usdc pool for boo outstanding
-contract = web3.eth.contract(address = ftmDict.SpookyswapStaking.getAddr(), abi = ftmDict.SpookyswapStaking.getABI())
-print(toNormal(contract.caller.pendingBOO(2, myAddress)))
+contract = w3.eth.contract(address = ftmDict.SpookyswapStaking.getAddr(), abi = ftmDict.SpookyswapStaking.getABI())
+print(toNormal(contract.functions.pendingBOO(2, me).call()))
 terminate.exitProcedure()
--- scripts/connect.py
+++ scripts/connect.py
@@ -3,23 +3,23 @@ import environment.getenv
 from scripts import terminate
 
 def fantom():
-    web3 = Web3(Web3.HTTPProvider(environment.getenv.ftm_API()))
-    return evaluate(web3)
+    w3 = Web3(Web3.HTTPProvider(environment.getenv.ftm_API()))
+    return evaluate(w3)
 
 def ethereum():
-    web3 = Web3(Web3.HTTPProvider(environment.getenv.ethereum_API()))
-    return evaluate(web3)
+    w3 = Web3(Web3.HTTPProvider(environment.getenv.ethereum_API()))
+    return evaluate(w3)
 
 def matic():
-    web3 = Web3(Web3.HTTPProvider(environment.getenv.matic_API()))
-    return evaluate(web3)
+    w3 = Web3(Web3.HTTPProvider(environment.getenv.matic_API()))
+    return evaluate(w3)
 
-def evaluate(web3):
-    connected = web3.isConnected()
+def evaluate(w3):
+    connected = w3.isConnected()
 
     if connected == False:
-        print('failed :: web3 RPC connection has failed...')
+        print('failed :: w3:: RPC connection has failed...')
         terminate.exitProcedure()
     else:
-        print('RPC Connected: ' + str(web3.isConnected()))
-        return web3
+        print('RPC Connected: ' + str(w3.isConnected()))
+        return w3
