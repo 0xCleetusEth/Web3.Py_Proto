8dda73ca305534cdc8fbb59213a6323bc64cc016
Chevis || Cleetus <sandysallahurst@gmail.com>
2021-09-11 03:10:54 -0500

    updated bare bones proto to include ideas to inspire friends

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import web3
 from environment import getenv
 from scripts import connect, terminate
 from registry import ftmDict
-
+import scripts.spookyfarm
 
 def toNormal(_inWei):
     _inNormal = w3.fromWei(_inWei, 'ether')
@@ -13,11 +13,17 @@ def toNormal(_inWei):
 w3 = connect.fantom()
 me = getenv.publicKey()
 
-
 balance = w3.eth.getBalance(me)
 print(toNormal(balance))
     
 #initializes spookychef interface, checks ftm-usdc pool for boo outstanding
 contract = w3.eth.contract(address = ftmDict.SpookyswapStaking.getAddr(), abi = ftmDict.SpookyswapStaking.getABI())
 print(toNormal(contract.functions.pendingBOO(2, me).call()))
-terminate.exitProcedure()
+
+# Call script here, such as spookyfarm.py - loop in script
+if w3.isConnected():
+    try:
+        scripts.spookyfarm.farm()
+    except:
+        terminate.exitProcedure()
+
---
+++ scripts/spookyfarm.py
@@ -0,0 +1,10 @@
+from environment.getenv import privateKey, publicKey
+import web3
+from environment import getenv
+
+# do stuff here, feel free to create w3 objects/ instances for scripting
+def farm():
+    while True:
+        return True
+        
+
